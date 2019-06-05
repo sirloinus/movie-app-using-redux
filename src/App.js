a65482ce4d320dc4722a6b8f447d8e563b609c03
sirloinus <amaliesmidth@gmail.com>
2019-06-05 10:31:36 +0100

    upgraded package.json to include latest dependencies

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {
   Switch,
   Link,
 } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
+
 
 const App = () => (
   <Router>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <div>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </div>
+  , document.getElementById('root'),
+);
 registerServiceWorker();
