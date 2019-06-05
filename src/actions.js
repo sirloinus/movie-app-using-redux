21527f575bced88c72c91d52d5bbf1f33cddfb2d
sirloinus <amaliesmidth@gmail.com>
2019-06-05 17:02:59 +0100

    created action-type constants

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,7 @@
+export const TOGGLE_MESSAGE = 'TOGGLE_MESSAGE';
+// created a string const TOGGLE_MESSAGE to protect code: if mistype happens elsewhere app won't run
+// leaving as a string means app will run but toggle won't work...
+
 export function toggleMessage() {
   return {
     type: 'TOGGLE_MESSAGE',
