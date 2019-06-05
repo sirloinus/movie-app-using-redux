324e7813d0a28c3819209443c122750f48a41a48
sirloinus <amaliesmidth@gmail.com>
2019-06-05 15:21:17 +0100

    added toggle feature using redux

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Toggle = ({ messageVisibility }) => (
+const Toggle = ({ messageVisibility, dispatch }) => (
+  // dispatch is being brought in as a prop because of connect()
+  // connecting the Toggle component gives us access to dispatch
+
   <div>
+
+    {/* dispatch is a function accepting an object (an action) as an argument */}
+
+    <button onClick={() => dispatch({
+        type: 'TOGGLE_MESSAGE',
+    })}
+
+    // the action type 'TOGGLE_MESSAGE' triggers the reducer
+
+    >Toggle Me!
+    </button>
+
     { messageVisibility &&
     <p>reduxxxxx innit</p>
     }
-    <button>Toggle</button>
+
   </div>
 );
 
