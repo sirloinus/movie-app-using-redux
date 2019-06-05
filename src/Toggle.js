cc5a953dc586f7a2dd77266fc5bd10799da301d5
sirloinus <amaliesmidth@gmail.com>
2019-06-05 15:45:32 +0100

    refactored by extracting action from dispatch in toggle to its own action.js file

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { toggleMessage } from './actions';
 
 const Toggle = ({ messageVisibility, dispatch }) => (
   // dispatch is being brought in as a prop because of connect()
@@ -8,14 +9,9 @@ const Toggle = ({ messageVisibility, dispatch }) => (
   <div>
 
     {/* dispatch is a function accepting an object (an action) as an argument */}
+    {/* the function toggleMessage is passed into dispatch and triggers the reducer */}
 
-    <button onClick={() => dispatch({
-        type: 'TOGGLE_MESSAGE',
-    })}
-
-    // the action type 'TOGGLE_MESSAGE' triggers the reducer
-
-    >Toggle Me!
+    <button onClick={() => dispatch(toggleMessage())}>Toggle Me!
     </button>
 
     { messageVisibility &&
